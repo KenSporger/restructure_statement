b56b0c6a13dea0ffe5944f50bc4ddc2d37b110a6
luxiuqiang <luxiuqiang@hikvision.com.cn>
2021-08-26 21:46:42 +0800

    将参数包装为对象StatementPerf、StatementData

--- v4/statement.h
+++ v4/statement.h
@@ -3,6 +3,23 @@
 
 #include "../types.h"
 
+struct StatementPerf : public Performance
+{
+	StatementPerf(const Performance& perf)
+		:Performance(perf) {}
+	Play play;
+	double amount;
+	int volumeCredits;
+};
+
+struct StatementData
+{
+	int totalVolumeCredits;
+	double totalAmount;
+	std::string customer;
+	std::vector<StatementPerf> performances;
+};
+
 std::string statement(const Invoice &invoices, const Plays &plays);
 
 #endif
